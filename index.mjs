ee3340202ca2eee46ec4e3e21549c821dfda7297
andrea-agosta-salt <andrea.agosta@appliedtechnology.se>
2022-06-27 12:55:09 +0200

    Add function getAgeGroup and add automated test

--- index.mjs
+++ index.mjs
@@ -1,1 +1,25 @@
 export const getAge = (birthYear, currentYear) => currentYear-birthYear;
+
+export const getAgeGroup = (age) => {
+    let regExpAge = new RegExp("[0-1][0-3][0-9]|[0-9][0-9]|[0-9]");
+
+    //return error if age is not a number or is < 0
+    if( !regExpAge.test(age) || age < 0){
+        return 'error';
+    } else {
+        
+        //check age e return the value
+        if(age < 4)
+            return 'toddler'
+        if(age < 13)
+            return 'kid';
+        if(age < 20)
+            return 'teenager';
+        if(age < 39)
+            return 'adult';
+        if(age === 50)
+            return 'prime'
+            
+        return 'old';
+    }
+};
--- tests.mjs
+++ tests.mjs
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { getAge } from './index.mjs';
+import { getAge, getAgeGroup } from './index.mjs';
 
 describe('age calculator', () => {
     it('someone born 1972 is 50 2022', () => {  
@@ -25,5 +25,103 @@ describe('age calculator', () => {
 
         //assert
         assert.equal(result, 37);
+    });
+});
+
+describe('age classifier', () => {
+    it('0-3 years old should be a toddler', () => {
+        //arrange
+        //act
+        const result0 = getAgeGroup(0);
+        const result1 = getAgeGroup(1);
+        const result2 = getAgeGroup(2);
+        const result3 = getAgeGroup(3);
+
+        //assert
+        assert.equal(result0, "toddler");
+        assert.equal(result1, "toddler");
+        assert.equal(result2, "toddler");
+        assert.equal(result3, "toddler");
+    });
+
+    it('4-12 years old should be a kid', () => {
+        //arrange
+        //act
+        const result = getAgeGroup(4);
+
+        //assert
+        assert.equal(result, "kid");
+    });
+
+    it('13-19 years old should be a teenager', () => {
+        //arrange
+        //act
+        const result = getAgeGroup(13);
+
+        //assert
+        assert.equal(result, 'teenager');
+    });
+
+    it('20-39 years old should be a adult',() => {
+        //arrange
+        //act
+        const result = getAgeGroup(38);
+
+        //assert
+        assert.equal(result, 'adult');
+    });
+
+    it('above 39 years old ... you are just old', () =>{
+        //arrange
+        //act
+        const result = getAgeGroup(40);
+
+        //assert
+        assert.equal(result, 'old');
+    });
+
+    it('But 50 - that is prime age, my friend. PRIME. AGE.', () => {
+        //arrange
+        //act
+        const result = getAgeGroup(50);
+
+        //assert
+        assert.equal(result, 'prime');
+    });
+
+    it('Error age < 0', () => {
+        //arrange
+        //act
+        const result = getAgeGroup(-1);
+
+        //assert
+        assert.equal(result, 'error');
+    });
+
+    it('Error space input type', () => {
+        //arrange
+        //act
+        const result = getAgeGroup(' ');
+
+        //assert
+        assert.equal(result, 'error');
+    });
+
+    it('Error no input type', () => {
+        //arrange
+        //act
+        const result = getAgeGroup('');
+
+        //assert
+        assert.equal(result, 'error');
+    });
+
+    it('Error text input type', () => {
+        //arrange
+        //act
+        const result = getAgeGroup('some text');
+
+        //assert
+        assert.equal(result, 'error');
     });
 });
