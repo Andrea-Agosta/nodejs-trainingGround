051c171eef7777395d5c740039e258d6bab89c50
andrea-agosta-salt <andrea.agosta@appliedtechnology.se>
2022-06-27 13:24:34 +0200

    Add test after variables course section

--- tests.mjs
+++ tests.mjs
@@ -1,127 +1,169 @@
-import assert from 'assert';
-import { getAge, getAgeGroup } from './index.mjs';
+import assert from "assert";
+import { getAge, getAgeGroup } from "./index.mjs";
 
-describe('age calculator', () => {
-    it('someone born 1972 is 50 2022', () => {  
-        //act
-        const result = getAge(1972, 2022);
-        
-        //assert
-        assert.equal(result, 50);
-    });
+describe("age calculator", () => {
+  it("someone born 1972 is 50 2022", () => {
+    //act
+    const result = getAge(1972, 2022);
 
-    it('someone born 2021 is 1 2022', () => {
-        // arrange
-        // act
-        const result = getAge(2021, 2022);
-      
-        // assert
-        assert.equal(result, 1);
-    });
+    //assert
+    assert.equal(result, 50);
+  });
 
-    it('someone born 1985 is 37 2022', () => {
-        // arrange
-        const result = getAge(1985, 2022);
+  it("someone born 2021 is 1 2022", () => {
+    // arrange
+    // act
+    const result = getAge(2021, 2022);
 
-        //assert
-        assert.equal(result, 37);
-    });
+    // assert
+    assert.equal(result, 1);
+  });
+
+  it("someone born 1985 is 37 2022", () => {
+    // arrange
+    const result = getAge(1985, 2022);
+
+    //assert
+    assert.equal(result, 37);
+  });
 });
 
-describe('age classifier', () => {
-    it('0-3 years old should be a toddler', () => {
-        //arrange
-        //act
-        const result0 = getAgeGroup(0);
-        const result1 = getAgeGroup(1);
-        const result2 = getAgeGroup(2);
-        const result3 = getAgeGroup(3);
+describe("age classifier", () => {
+  it("0-3 years old should be a toddler", () => {
+    //arrange
+    //act
+    const result0 = getAgeGroup(0);
+    const result1 = getAgeGroup(1);
+    const result2 = getAgeGroup(2);
+    const result3 = getAgeGroup(3);
 
-        //assert
-        assert.equal(result0, "toddler");
-        assert.equal(result1, "toddler");
-        assert.equal(result2, "toddler");
-        assert.equal(result3, "toddler");
-    });
+    //assert
+    assert.equal(result0, "toddler");
+    assert.equal(result1, "toddler");
+    assert.equal(result2, "toddler");
+    assert.equal(result3, "toddler");
+  });
 
-    it('4-12 years old should be a kid', () => {
-        //arrange
-        //act
-        const result = getAgeGroup(4);
+  it("4-12 years old should be a kid", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(4);
 
-        //assert
-        assert.equal(result, "kid");
-    });
+    //assert
+    assert.equal(result, "kid");
+  });
 
-    it('13-19 years old should be a teenager', () => {
-        //arrange
-        //act
-        const result = getAgeGroup(13);
+  it("13-19 years old should be a teenager", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(13);
 
-        //assert
-        assert.equal(result, 'teenager');
-    });
+    //assert
+    assert.equal(result, "teenager");
+  });
 
-    it('20-39 years old should be a adult',() => {
-        //arrange
-        //act
-        const result = getAgeGroup(38);
+  it("20-39 years old should be a adult", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(38);
 
-        //assert
-        assert.equal(result, 'adult');
-    });
+    //assert
+    assert.equal(result, "adult");
+  });
 
-    it('above 39 years old ... you are just old', () =>{
-        //arrange
-        //act
-        const result = getAgeGroup(40);
+  it("above 39 years old ... you are just old", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(40);
 
-        //assert
-        assert.equal(result, 'old');
-    });
+    //assert
+    assert.equal(result, "old");
+  });
 
-    it('But 50 - that is prime age, my friend. PRIME. AGE.', () => {
-        //arrange
-        //act
-        const result = getAgeGroup(50);
+  it("But 50 - that is prime age, my friend. PRIME. AGE.", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(50);
 
-        //assert
-        assert.equal(result, 'prime');
-    });
+    //assert
+    assert.equal(result, "prime");
+  });
 
-    it('Error age < 0', () => {
-        //arrange
-        //act
-        const result = getAgeGroup(-1);
+  it("Error age < 0", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(-1);
 
-        //assert
-        assert.equal(result, 'error');
-    });
+    //assert
+    assert.equal(result, "error");
+  });
 
-    it('Error space input type', () => {
-        //arrange
-        //act
-        const result = getAgeGroup(' ');
+  it("Error space input type", () => {
+    //arrange
+    //act
+    const result = getAgeGroup(" ");
 
-        //assert
-        assert.equal(result, 'error');
-    });
+    //assert
+    assert.equal(result, "error");
+  });
 
-    it('Error no input type', () => {
-        //arrange
-        //act
-        const result = getAgeGroup('');
+  it("Error no input type", () => {
+    //arrange
+    //act
+    const result = getAgeGroup("");
 
-        //assert
-        assert.equal(result, 'error');
-    });
+    //assert
+    assert.equal(result, "error");
+  });
 
-    it('Error text input type', () => {
-        //arrange
-        //act
-        const result = getAgeGroup('some text');
+  it("Error text input type", () => {
+    //arrange
+    //act
+    const result = getAgeGroup("some text");
 
-        //assert
-        assert.equal(result, 'error');
-    });
+    //assert
+    assert.equal(result, "error");
+  });
+});
+
+describe("variable section", () => {
+  it.skip("you cannot change a const variable", () => {
+    const aNumber = 1;
+    aNumber = 2;
+  });
+
+  it("you can change a let variable", () => {
+    // arrange
+    let aNumber = 1;
+
+    // act
+    aNumber = 2;
+
+    // assert
+    assert.equal(aNumber, 2);
+  });
+
+  it("uninitialized variables are undefined", () => {
+    // act
+    let aNumber;
+
+    // assert
+    assert.equal("undefined", typeof aNumber);
+    assert.equal(undefined, aNumber);
+  });
+
+  it("null should be used for not set", () => {
+    // arrange
+    // declaring a dummy function
+    const peekIntoTheBox = () => {
+      return false;
+    };
+
+    // act
+    let isCatAlive = null;
+
+    // assert
+    assert.equal(null, isCatAlive);
+    isCatAlive = peekIntoTheBox();
+  });
 });
